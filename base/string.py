10e24c449bf2f8334ef4962b3efee9345a442072
Lousic Lee <liyimpc@icloud.com>
2018-06-14 09:17:10 +0800

    元组，字符串
    元组，字符串

--- base/string.py
+++ base/string.py
@@ -78,4 +78,102 @@ print(ord("a"))
 # ascii转换为字符
 print(chr(65))
 
+# str.split(str="", num)
+# 截出来的数据放到列表里
+str38 = "sunk**is*a**good*man"
+print(str38.split("*"))
+# num：仅截取num次*
+print(str38.split("*", 4))
 
+# splitlines 按照('\r', '\r\n', '\n')分割
+# keepends = True 会保留换行符, 默认False
+str40 = '''
+lym is a good man
+lym is a nice man 
+lym is a handsome man
+
+'''
+print(str40.splitlines())
+print(str40.splitlines(True))
+
+# join(seq) 以指定的字符串分隔符，将seq中的所有元组组合成一个字符串
+list41 = ['sunk', 'is', 'a', 'good', 'man']
+str42 = "&".join(list41)
+print(str42)
+
+# max() min(), 比较按照ascii来比较
+str43 = "lym is a good man"
+print(max(str43))
+print("*"+min(str43)+"*")
+
+# replace(oldstr, newstr, count)
+# 用newstr替换oldstr,默认是全部替换，如果指定了count,只替换count个
+str44 = "lym is a good man"
+str45 = str44.replace("good", "nice", 1)
+print(str45)
+
+# str.makestrans(oldstr, newstr)
+# 创建一个字符串映射表
+# translate() 结合映射表一起使用
+t46 = str.maketrans("ac", "65")
+# 映射规则：a->6 c->5
+str47 = "lym is a good man good cca"
+str48 = str47.translate(t46)
+print(str48)
+
+# startswith(str, start=0, end=len(str))
+# 在给定的范围内，是否是以给定的字符串开头，如果没有指定范围，默认整个字符串
+str49 = "lym is a good man"
+print(str49.startswith("lym", 0, 5))
+
+# endswith(str, start=0, end=len(str))
+# 在给定的范围内，是否是以给定的字符串结尾，如果没有指定范围，默认整个字符串
+print(str49.endswith("lym", 0, 5))
+
+# encode(encoding="UTF-8", errors="strict")
+# 编码
+str50 = "lym is a good man 李一鸣"
+data52 = str50.encode("utf-8", "ignore")
+print(data52)
+
+# decode(encoding="UTF-8", errors="strict")
+# 解码   注意：要与编码格式一致
+str53 = data52.decode("utf-8", "ignore")
+print(str53)
+
+# isalpha()
+# 如果字符串中至少有一个字符且所有的字符都是字母,返回True,否则返回False
+str54 = "lym is a good man"
+print(str54.isalpha())
+
+# isalnum()
+# 如果字符串中至少有一个字符且所有的字符都是数字，返回True，否则返回False
+str55 = "123"
+print(str55.isalnum())
+
+# isupper()
+# 如果字符串中至少有一个字符且所有的字符都是大写，返回True，否则返回False
+str56 = "LYM"
+print(str56.isupper())
+
+# istitle()
+# 如果字符串是标题化的,返回True，否则返回False
+# 标题化：首字母大写,其余字母小写
+print("Lym Is".istitle())
+
+# isdgit()
+# 如果字符串只包含数字字符，返回True,否则返回False
+print("123".isdigit())
+
+# isnumeric()
+# 如果字符串只包含数字，返回True,否则返回False
+print("123".isnumeric())
+
+# isdecimal()
+# 如果字符串只包含十进制字符，返回True,否则返回False
+print("123".isdecimal())
+
+# isspace()
+# 如果字符串只包空格，返回True,否则返回False
+# \t \n \r
+print("\t".isspace())
